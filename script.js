e64304d3cd7c9ef5e496bda3067eb2f94c8362cb
Sothyra Chan <schan256@my.centennialcollege.ca>
2024-12-04 01:00:47 -0500

    using Number() to convert a current string input value into an array of numbers

--- script.js
+++ script.js
@@ -2,4 +2,11 @@
 
 const calculate = () => {
     const value = document.querySelector('#number').value;
+
+    // split() takes a string or regExp and splits it into an array of strings
+    const array = value.split(/,\s*/g);
+
+    // map() converts the array of strings and returns a new array without mutating the original array
+    // Number() converts the string into a number of a current input value
+    const numbers = array.map(value => Number(value));
 };
