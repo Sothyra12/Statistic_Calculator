800b60c7a5625ee1d42470ddd83fa43059793787
Sothyra Chan <schan256@my.centennialcollege.ca>
2024-12-04 19:01:27 -0500

    created a getMean function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // script.js
 
+// reduce() takes 2 arguments & the return value for the callback function is the accumulator on the next iteration
+// the initial value is set to 0 to be safely added to the first element of the array 
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length
+
 const calculate = () => {
     const value = document.querySelector('#number').value;
 
@@ -8,5 +12,11 @@ const calculate = () => {
 
     // map() converts the array of strings and returns a new array without mutating the original array
     // Number() converts the string into a number of a current input value
-    const numbers = array.map(value => Number(value));
+    // filter() out the inputs that are not numbers
+    // use the isNaN() function to check if the input is not a number
+    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+
+    const mean = getMean(numbers);
+    // display the mean value
+    document.querySelector("#mean").textContent = mean;
 };
